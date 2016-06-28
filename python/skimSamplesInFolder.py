073bf9711112b1541e081377eb0b3306fd0c5354
knutzen <knutzen@physik.rwth-aachen.de>
2016-06-28 11:19:41 +0200

    skimSamplesInFolder: Script to execute the skim tool for files in dir.
    This script will execute the skim tool for all samples in a stated
    directory.

---
+++ python/skimSamplesInFolder.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import optparse
+import shutil
+import subprocess
+
+def readInput():
+    parser = optparse.OptionParser( description='Perform the skimming for all samples in a folder', \
+                                    usage='usage: %prog [options] folder ' )
+    parser.add_option( '-c', '--config', action='store', metavar='config', default="$CMSSW_BASE/src/Validation/RecoTau/Tools/GetRecoTauVFromDQM_MC_cff.py", dest="config",
+                       help='Additional filter on the filenames. [default: %default]' )
+    parser.add_option( '-o', '--output_dir', action='store', metavar='output_dir', default="/disk1/knutzen/TauPOG/RelVal/samples/", dest="output_dir",
+                       help='Output directory. [default: %default]' )
+
+    ( options, args ) = parser.parse_args()
+
+    return options, args[ 0 ]
+
+
+
+def main():
+    options, path = readInput()
+
+    input_list = os.listdir( path )
+
+    for sample in input_list:
+        sample_name_new = sample.split("__RelVal")[ -1 ]
+
+        postfix = ""
+        if "ZTT" in sample_name_new or "Zprime" in sample_name_new:
+            postfix = "ZTT"
+        if "ZMM" in sample_name_new:
+            postfix = "ZMM"
+        if "ZEE" in sample_name_new:
+            postfix = "ZEE"
+        if "TTbar" in sample_name_new or "QCD" in sample_name_new:
+            postfix = "QCD"
+
+        exe = "python " + options.config + " " + path + "/" + sample + " " + path + "/"  + sample_name_new + " " + postfix
+        #print exe
+        p = subprocess.Popen( exe, \
+        stdout=subprocess.PIPE, shell=True, stderr=subprocess.PIPE, stdin = subprocess.PIPE )
+        ( out, err ) = p.communicate()
+
+
+if __name__ == '__main__':
+    main()
