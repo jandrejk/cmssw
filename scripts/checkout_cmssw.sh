15a9c19e6f8635390d18eb6bd6fe5e39b5694aec
Thomas Mueller <tmuller@cern.ch>
2017-01-23 17:14:44 +0100

    checkout_cmssw.sh: checkout new TAU POG development branch

--- scripts/checkout_cmssw.sh
+++ scripts/checkout_cmssw.sh
@@ -9,6 +9,12 @@ cd CMSSW_9_0_0_pre1/src
 eval `scramv1 runtime -sh`
 
 git cms-addpkg Validation/RecoTau
+cd Validation/RecoTau
+git remote add -f cms-tau-pog https://github.com/cms-tau-pog/cmssw.git
+git fetch cms-tau-pog/CMSSW_9_0_X_tau-pog_validation-tools
+git checkout cms-tau-pog/CMSSW_9_0_X_tau-pog_validation-tools
+cd $CMSSW_BASE/src
+
 git clone https://gitlab.cern.ch/knutzen/tau_validation_tools.git Validation/tau_validation_tools
 
 scramv1 b -j 4
