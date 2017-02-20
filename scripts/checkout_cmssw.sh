9f8b33a4ed673eae43682ab94866a1e11be52bc5
Thomas Mueller <tmuller@cern.ch>
2017-02-20 16:24:23 +0100

    checkout_cmssw.sh: get things working

--- scripts/checkout_cmssw.sh
+++ scripts/checkout_cmssw.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# https://twiki.cern.ch/twiki/bin/viewauth/CMS/SWGuidePFTauIDDevelopers#How_to_set_up_CMSSW_for_my_devel
 
 export SCRAM_ARCH=slc6_amd64_gcc530
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
@@ -7,14 +8,14 @@ source $VO_CMS_SW_DIR/cmsset_default.sh
 scramv1 project CMSSW CMSSW_9_0_0_pre1;
 cd CMSSW_9_0_0_pre1/src
 eval `scramv1 runtime -sh`
+git cms-init
 
 git cms-addpkg Validation/RecoTau
-cd Validation/RecoTau
-git remote add -f cms-tau-pog https://github.com/cms-tau-pog/cmssw.git
-git fetch cms-tau-pog/CMSSW_9_0_X_tau-pog_validation-tools
-git checkout cms-tau-pog/CMSSW_9_0_X_tau-pog_validation-tools
-cd $CMSSW_BASE/src
+git remote add tau-pog https://github.com/cms-tau-pog/cmssw.git
+git remote add my-tau-pog https://github.com/thomas-mueller/cmssw.git
+git fetch my-tau-pog CMSSW_9_0_X_tau-pog_validation-tools:CMSSW_9_0_X_tau-pog_validation-tools
+git checkout CMSSW_9_0_X_tau-pog_validation-tools
 
 git clone https://gitlab.cern.ch/knutzen/tau_validation_tools.git Validation/tau_validation_tools
 
-scramv1 b -j 4
+scramv1 b -j 8
