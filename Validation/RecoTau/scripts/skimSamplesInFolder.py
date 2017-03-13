7f41181c05590ca37acd165317b81f65da0df00f
Thomas Mueller <tmuller@cern.ch>
2017-03-13 16:38:57 +0100

    skimSamplesInFolder.py: added parallelisation functionality

--- Validation/RecoTau/scripts/skimSamplesInFolder.py
+++ Validation/RecoTau/scripts/skimSamplesInFolder.py
@@ -7,6 +7,9 @@ import sys
 import shutil
 import subprocess
 
+import Validation.RecoTau.tools as tools
+
+
 def readInput():
 	parser = argparse.ArgumentParser(description="Perform the skimming for all samples in a folder")
 	parser.add_argument("path",
@@ -18,6 +21,8 @@ def readInput():
 	parser.add_argument("-o", "--output-dir",
 	                    default="$CMSSW_BASE/src/Validation/RecoTau/data/relval_skims",
 	                    help="Output directory. [default: %(default)s]")
+	parser.add_argument("-n", "--n-processes", type=int, default=1,
+	                    help="Number of parallel processes. [default: %(default)s]")
 
 	args = parser.parse_args()
 	args.config = os.path.expandvars(args.config)
@@ -32,6 +37,7 @@ def main():
 	input_list = glob.glob(os.path.join(args.path, "*.root"))
 	campaign = args.path[args.path.rfind("CMSSW"):]
 
+	commands = []
 	for input_file in input_list:
 		output_file = os.path.join(args.output_dir, campaign, os.path.basename(input_file))
 		if not os.path.exists(os.path.dirname(output_file)):
@@ -47,13 +53,9 @@ def main():
 		if "TTbar" in input_file or "QCD" in input_file:
 			postfix = "QCD"
 
-		exe = "python " + args.config + " " + input_file + " " + output_file + " " + postfix
-		print exe
-		p = subprocess.Popen(exe,
-		stdout=subprocess.PIPE, shell=True, stderr=subprocess.PIPE, stdin = subprocess.PIPE)
-		(out, err) = p.communicate()
-		print out
-		print err
+		commands.append("python " + args.config + " " + input_file + " " + output_file + " " + postfix)
+	
+	tools.parallelize(tools.call_command, commands, n_processes=args.n_processes)
 
 
 if __name__ == "__main__":
